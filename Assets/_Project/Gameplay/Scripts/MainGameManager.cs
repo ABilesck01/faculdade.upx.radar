5286fca804edf781cbc2115e12dc362e5878b063
ABilesck <alex.bilesck@gmail.com>
2023-06-01 10:44:40 -0300

    added leaderboard screen

--- Assets/_Project/Gameplay/Scripts/MainGameManager.cs
+++ Assets/_Project/Gameplay/Scripts/MainGameManager.cs
@@ -1,8 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainGameManager : MonoBehaviour
 {
     public static string SceneName = "Game";
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SceneManager.LoadScene($"Leaderboard", LoadSceneMode.Additive);
+        }
+    }
 }
